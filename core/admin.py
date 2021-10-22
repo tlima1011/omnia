6337b0f604f3bba76bfcd1592283826f7cf1ed16
Thiago <thiagolima1011@gmail.com>
2021-10-21 23:32:23 -0300

    Criacao de modelo e banco de dados com usuario

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Produto
 
 # Register your models here.
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'ncm', 'valor', 'data_criacao')
+    list_filter = ('usuario', 'nome',)
+
+admin.site.register(Produto, ProdutoAdmin)
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+    ncm = models.IntegerField()
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+    data_criacao = models.DateField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table ='produto'
+
+    def __str__(self):
+        return self.nome
+
