9c9279f83ed8501d17f10702910f5c378f775614
Thiago <thiagolima1011@gmail.com>
2021-10-22 14:17:20 -0300

    Insercao de tela de login e consistencias

--- core/views.py
+++ core/views.py
@@ -1,13 +1,40 @@
 from django.shortcuts import render, redirect
 from core.models import Produto
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 #def index(request):
 #    return redirect('/produto/')
 
+
+def login_user(request):
+    return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, 'Usuario e/ou senha invalidos')
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
 def lista_produtos(request):
-    #usuario = request.user
-    produto = Produto.objects.all()
+    usuario = request.user
+    produto = Produto.objects.filter(usuario=usuario)
     dados = {'produto': produto}
     return render(request, 'produto.html', dados)
 
